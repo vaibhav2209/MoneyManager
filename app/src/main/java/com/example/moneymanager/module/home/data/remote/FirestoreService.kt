00206505a2260620bdf1b2fce07cda9271e952e1
Vaibhav Patel <vaibhav.mobilefirst@gmail.com>
2022-04-07 15:39:21 +0530

    Data store

---
+++ app/src/main/java/com/example/moneymanager/module/home/data/remote/FirestoreService.kt
@@ -0,0 +1,15 @@
+package com.example.moneymanager.module.home.data.remote
+
+import com.example.moneymanager.module.auth.domain.model.RegisterUser
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.firestore.FirebaseFirestore
+
+class FirestoreService(
+    private val db : FirebaseFirestore
+){
+
+
+}
